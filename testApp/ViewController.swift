0ee55c7681fb20680bc41aad015086a7c3dfab74
Tokuda Shusuke <tottokotkd@tottokoMBP.local>
2015-01-01 23:41:17 +0900

    Initial Commit

---
+++ testApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testApp
+//
+//  Created by Tokuda Shusuke on 2015/01/01.
+//  Copyright (c) 2015年 tottokotkd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
